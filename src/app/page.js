68dfcc11f5efce364da642f3dcb354a9166ef5c1
PlayGamesMakeGames <tjlakatos89@gmail.com>
2025-04-18 12:24:31 -0400

    It looks cool now, with headings!

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-     <h1>
+     <h1 className="title">
         Adding Projects To Your Resume as a Tech Major Through Game Development
      </h1>
      <div className="resourceDropdown">
@@ -21,19 +21,24 @@ export default function Home() {
         </ul>
      </div>
      <div className="whatInGuide section" id="1">
-        Here is what is in the guide
+       <h1 className="title">What's in the guide?</h1>
+       Here is what is in the guide
      </div>
      <div className="whoGuideFor section" id="2">
-        This guide is for anyone in a tech major like Computer Science or Information System Technology who wants to add projects to their resume and wants to do it through game development.
+       <h1 className="title">Who is the guide for?</h1>
+       This guide is for anyone in a tech major like Computer Science or Information System Technology who wants to add projects to their resume and wants to do it through game development.
      </div>
      <div className="assumptions section" id="3">
-        Before using this guide, you should know at least 1 coding language like Java or Python. This will ensure you have an easy time learning how to use a game engine.
+       <h1 className="title">What should you already know?</h1>
+       Before using this guide, you should know at least 1 coding language like Java or Python. This will ensure you have an easy time learning how to use a game engine.
      </div>
      <div className="organized section" id="4">
-        This guide is organized as 6 unique resources that can help you become a game developer. They are in order of what you need to know to create your first game!
+       <h1 className="title">How is the guide organized?</h1>
+       This guide is organized as 6 unique resources that can help you become a game developer. They are in order of what you need to know to create your first game!
      </div>
      <div className="tips section" id="5">
-        Some tips for navigating this website: If you want to return to the top of the page, click the video game controller icon in the top left!
+       <h1 className="title">Tips for navigating this site</h1>
+       Some tips for navigating this website: If you want to return to the top of the page, click the video game controller icon in the top left!
      </div>
     </div>
   );
