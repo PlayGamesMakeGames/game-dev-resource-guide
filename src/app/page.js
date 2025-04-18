23e29caef1edbc92c2c69a0b8516bb82a36643e4
PlayGamesMakeGames <tjlakatos89@gmail.com>
2025-04-18 12:15:43 -0400

    Base of website made

--- src/app/page.js
+++ src/app/page.js
@@ -4,92 +4,37 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+     <h1>
+        Adding Projects To Your Resume as a Tech Major Through Game Development
+     </h1>
+     <div className="resourceDropdown">
+        Resources
+     </div>
+     <div className="tableOfContents">
+        <h1 className="title">Table of Contents</h1>
+        <ul className="tableOfContentsLinks">
+          <li><a href="#1">What's in the guide?</a></li>
+          <li><a href="#2">Who is the Guide for?</a></li>
+          <li><a href="#3">What should you already know?</a></li>
+          <li><a href="#4">How is the guide organized?</a></li>
+          <li><a href="#5">Tips for navigating this site</a></li>
+        </ul>
+     </div>
+     <div className="whatInGuide section" id="1">
+        Here is what is in the guide
+     </div>
+     <div className="whoGuideFor section" id="2">
+        This guide is for anyone in a tech major like Computer Science or Information System Technology who wants to add projects to their resume and wants to do it through game development.
+     </div>
+     <div className="assumptions section" id="3">
+        Before using this guide, you should know at least 1 coding language like Java or Python. This will ensure you have an easy time learning how to use a game engine.
+     </div>
+     <div className="organized section" id="4">
+        This guide is organized as 6 unique resources that can help you become a game developer. They are in order of what you need to know to create your first game!
+     </div>
+     <div className="tips section" id="5">
+        Some tips for navigating this website: If you want to return to the top of the page, click the video game controller icon in the top left!
+     </div>
     </div>
   );
 }
